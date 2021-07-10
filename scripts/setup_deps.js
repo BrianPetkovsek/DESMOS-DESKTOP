f9a9ddf86fefa509f6cdcf9c0a79ed5feb5aa93e
BrianPetkovsek <16124109+BrianPetkovsek@users.noreply.github.com>
2021-07-09 20:05:46 -0400

    add folders

--- scripts/setup_deps.js
+++ scripts/setup_deps.js
@@ -3,4 +3,7 @@ const fsPromises = fs.promises;
 
 (async () => {
 	await fsPromises.copyFile("AdvancedWebArchiver/arguments.txt", "arguments.txt").catch(err){}
+	fs.promises.mkdir('BACKUPS', { recursive: true }).catch(console.error);
+	fs.promises.mkdir('DATA_PATCHES', { recursive: true }).catch(console.error);
+	fs.promises.mkdir('LIVE_DATA_LINE', { recursive: true }).catch(console.error);
 })();
